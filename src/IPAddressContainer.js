3a045130c6fd7b015cd298a70ba939425ff05de7
Andrii <vytiaganets@ukr.net>
2020-01-10 14:49:52 +0200

    IP Address

---
+++ src/IPAddressContainer.js
@@ -0,0 +1,41 @@
+import React, {Component} from "react";
+import IPAddress from "./IPAddress";
+
+var xhr;
+
+class IPAddressContainer extends Component {
+  constructor(props, contexts) {
+    super(props, contexts);
+
+    this.state = {
+      ip_address: "..."
+    };
+    this.processRequest = this.processRequest.bind(this);
+  }
+
+  componentDidMount() {
+    xhr = new XMLHttpRequest();
+    xhr.open('GET', "https://ipinfo.io/json", true);
+    xhr.send();
+
+    xhr.addEventListener("readystatechange",
+    this.processRequest, false);
+  }
+
+  processRequest() {
+    if(xhr.readyState === 4 && xhr.status === 200) {
+      var response = JSON.parse(xhr.responseText);
+
+      this.setState({
+        ip_address: response.ip
+      });
+    } 
+  }
+  render() {
+    return (
+    <IPAddress ip={this.state.ip_address}/>
+    );
+  }
+};
+
+export default IPAddressContainer;
